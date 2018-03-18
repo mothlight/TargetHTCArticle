b899de6a4d06534b5f5015ef71f7252fff8ad6d7
Kerry <kerry.nice@monash.edu>
2018-03-19 08:50:21 +1100

    Initial input

---
+++ compileArticle.sh
@@ -0,0 +1,11 @@
+cp -u ../Bibtex/PhDMendeley.bib/library.bib .
+#cp -u /home/kerryn/Documents/Work/VTUF-Runs/PrestonBase/Analysis/Pr04Val-ErrorPlots.png images/Pr04Val-ErrorPlots.png
+
+
+bibtex TargetHtc.aux
+makeglossaries  TargetHtc
+#pdflatex -file-line-error -synctex=1 -interaction=nonstopmode TargetHtc.tex
+makeindex VTUF-TargetHtc.nlo -s nomencl.ist -o TargetHtc.nls
+pdflatex -file-line-error -synctex=1 -interaction=errorstopmode --shell-escape TargetHtc.tex
+#makeindex elsarticle-template.nlo -s nomencl.ist -o elsarticle-template.nls
+bibtex TargetHtc.aux
