a820215e755e34cfafc54c8f7157d94d7118e1b4
Kerry <kerry.nice@monash.edu>
2018-09-27 17:26:03 +1000

    Add files to repository

--- compileArticle.sh
+++ compileArticle.sh
@@ -1,7 +1,7 @@
 cp -u ../Bibtex/PhDMendeley.bib/library.bib .
 #cp -u /home/kerryn/Documents/Work/VTUF-Runs/PrestonBase/Analysis/Pr04Val-ErrorPlots.png images/Pr04Val-ErrorPlots.png
 
-
+rm TargetHtc.glsdefs
 bibtex TargetHtc.aux
 makeglossaries  TargetHtc
 #pdflatex -file-line-error -synctex=1 -interaction=nonstopmode TargetHtc.tex
