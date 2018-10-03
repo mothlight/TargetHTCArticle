a62517a98b140eff68fc96f20758eb4c6387419f
Kerry <kerry.nice@monash.edu>
2018-10-03 13:49:30 +1000

    Adding gipps/george st evals

--- compileArticle.sh
+++ compileArticle.sh
@@ -1,6 +1,15 @@
 cp -u ../Bibtex/PhDMendeley.bib/library.bib .
 #cp -u /home/kerryn/Documents/Work/VTUF-Runs/PrestonBase/Analysis/Pr04Val-ErrorPlots.png images/Pr04Val-ErrorPlots.png
 
+cp -u /home/kerryn/Documents/Work/Toolkit2-Runs/CoMUTCIValidation/Analysis/GippsEvaluation-ErrorPlots-UTCI.png images/Eval
+cp -u /home/kerryn/Documents/Work/Toolkit2-Runs/CoMUTCIValidation/Analysis/GeorgeEvaluation-ErrorPlots-Tair.png images/Eval
+cp -u /home/kerryn/Documents/Work/Toolkit2-Runs/CoMUTCIValidation/Analysis/GeorgeEvaluation-ErrorPlots-UTCI.png images/Eval
+cp -u /home/kerryn/Documents/Work/Toolkit2-Runs/CoMUTCIValidation/Analysis/GeorgeEvaluation-ErrorPlots-Tmrt.png images/Eval
+cp -u /home/kerryn/Documents/Work/Toolkit2-Runs/CoMUTCIValidation/Analysis/GippsEvaluation-ErrorPlots-Tmrt.png images/Eval
+cp -u /home/kerryn/Documents/Work/Toolkit2-Runs/CoMUTCIValidation/Analysis/GippsEvaluation-ErrorPlots-Tair.png images/Eval
+
+
+
 rm TargetHtc.glsdefs
 bibtex TargetHtc.aux
 makeglossaries  TargetHtc
